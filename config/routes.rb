9cbe28dc7f075e98c6fe11f306af6f2aa4a82016
Monier <m.monierx@gmail.com>
2024-02-14 00:17:00 +0200

    edit root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "movies#index"
+  root "movies#new_import"
   resources :actors, only: [:index, :show] do
     collection do
       get :search
